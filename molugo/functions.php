2c263d4f3d024ebf04167c7fdce9159a81b7c4ee
haubar <haubarclack@gmail.com>
2017-05-23 20:06:35 +0800

    updatge image name

--- molugo/functions.php
+++ molugo/functions.php
@@ -54,11 +54,11 @@ function getPostViews($postID) {
 //加入特色圖片
 function molugo_post_thumbnails() {
     add_theme_support( 'post-thumbnails' );
-    set_post_thumbnail_size( 367, 210); // default Featured Image dimensions (cropped)
+    set_post_thumbnail_size( 367, 210, true); // default Featured Image dimensions (cropped)
  
     // add_image_size( 'category-thumb', 300, 9999 ); // 300 pixels wide (and unlimited height)
-    add_image_size( 'single-thumb', 1000, 363 , true); // 
-    add_image_size( 'index-master-thumb', 576, 576, true ); // 
+    add_image_size( 'single-thumb', 1000, 363 ); // 
+    add_image_size( 'index-master-thumb', 576, 576 ); // 
     add_image_size( 'index-slave-thumb', 417, 276 ); // 
     add_image_size( 'index-center-thumb', 598, 399 ); // 
     add_image_size( 'list-thumb', 326, 217 ); // 
