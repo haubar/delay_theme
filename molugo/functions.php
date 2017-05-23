07d5057b255e9179d007cfa24629935e2c5f3d54
haubar <haubarclack@gmail.com>
2017-05-23 19:54:52 +0800

    fix image crop

--- molugo/functions.php
+++ molugo/functions.php
@@ -54,10 +54,10 @@ function getPostViews($postID) {
 //加入特色圖片
 function molugo_post_thumbnails() {
     add_theme_support( 'post-thumbnails' );
-    set_post_thumbnail_size( 367, 210 ); // default Featured Image dimensions (cropped)
+    set_post_thumbnail_size( 367, 210, true); // default Featured Image dimensions (cropped)
  
     // add_image_size( 'category-thumb', 300, 9999 ); // 300 pixels wide (and unlimited height)
-    add_image_size( 'single-thumb', 1000, 363 , true); // 300 pixels wide (and unlimited height)
+    add_image_size( 'single-thumb', 1000, 363 ); // 300 pixels wide (and unlimited height)
     add_image_size( 'index-master-thumb', 576, 576 ); // 300 pixels wide (and unlimited height)
     add_image_size( 'index-slave-thumb', 417, 276 ); // 300 pixels wide (and unlimited height)
     add_image_size( 'index-center-thumb', 598, 399 ); // 300 pixels wide (and unlimited height)
