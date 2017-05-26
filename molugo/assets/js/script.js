10bd16f8b0ec13f52a20244a2e51eb1ceb7e35b9
haubar <haubarclack@gmail.com>
2017-05-26 19:42:36 +0800

    update 側邊滑動速度

--- molugo/assets/js/script.js
+++ molugo/assets/js/script.js
@@ -39,9 +39,9 @@ jQuery(document).ready(function($) {
 	}
 	jQuery(window).scroll(function(){
 		if(banner === true){
-			jQuery('.footer.mb-h').stop().animate({top: jQuery(this).scrollTop()}, 500);
+			jQuery('.footer.mb-h').stop().animate({top: jQuery(this).scrollTop()}, 'normal');
 		}else{
-			jQuery('.footer.mb-h').stop().animate({top: jQuery(this).scrollTop() + 300}, 500);	
+			jQuery('.footer.mb-h').stop().animate({top: jQuery(this).scrollTop() + 300}, 'normal');	
 		}
 		
 	})
