cefe87fed98247bc415402b70be9549728c0f514
haubar <haubarclack@gmail.com>
2017-05-26 19:16:01 +0800

    fix footer

--- molugo/single.php
+++ molugo/single.php
@@ -90,6 +90,7 @@
 		<div class="footer mb-h">
 			<a href="#" class="fb"></a>
 			<a href="#" class="email"></a>
+			<a href="#" class="link"></a>
 			<img src="<?php bloginfo('template_directory') ?>/assets/images/copyright.png" alt="copyright">
 		</div>
 	</div>
