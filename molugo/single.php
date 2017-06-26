ca2481c32dc7c8cf962976e3b1df20b2b974cbe9
haubar <haubarclack@gmail.com>
2017-06-26 23:57:31 +0800

    add fb link

--- molugo/single.php
+++ molugo/single.php
@@ -88,7 +88,7 @@
 	<?php //wp_pagenavi(); ?>
 
 		<div class="footer mb-h">
-			<a href="#" class="fb"></a>
+			<a href="https://www.facebook.com/delayreport" class="fb"></a>
 			<a href="#" class="email"></a>
 			<a href="#" class="link"></a>
 			<img src="<?php bloginfo('template_directory') ?>/assets/images/copyright.png" alt="copyright">
